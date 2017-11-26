8aa895803c4f1646a99737ce2e4b763b0dcca7cd
Francis <francisbouletrouleau@hotmail.com>
2017-11-26 16:15:32 -0500

    better profile pics (in search_videograph.php)

--- search_videograph.php
+++ search_videograph.php
@@ -1,5 +1,14 @@
 <title>Searching videograph</title>
 <?php include "includes/header.php"; ?>
+<style type="text/css">
+.profile_pic {
+	width: 100%;
+	height: 80px;
+	background-size: cover;
+	background-repeat: no-repeat;
+	background-position: 50% 50%;
+};
+</style>
 
 
 <div class="container">
@@ -17,39 +26,44 @@
 	<h1 style="margin-bottom: 30px;">What we found:</h1>
 
 	<div class="row">
-		<div class="col-sm-4"> 
-			<div class="panel panel-primary" style="background-color: #000000">
-				<!-- <div class="panel-heading">BLACK FRIDAY DEAL</div> -->
-				<video class="panel-body video" style="width: 100%; margin: -14.87px; margin-left: 50%; transform: translateX(-50%);" id="bgVideo" loop  muted="" preload autoplay>
-					<source src="video1.mp4" type="video/mp4"></source>
-				</video>
-				<!-- <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%; margin: -15px; margin-left: 50%; transform: translateX(-50%); " alt="Image"></div> -->
+		<div class="col-sm-4">
+			<a href="#" style="color: #000000">
+				<div class="panel panel-primary" style="background-color: #000000">
+					<video class="panel-body video" style="width: 100%; margin: -14.87px; margin-left: 50%; transform: translateX(-50%);" id="bgVideo" loop  muted="" preload autoplay>
+						<source src="video1.mp4" type="video/mp4"></source>
+					</video>
 
-				<div class="panel-footer">
-					<div class="row">
-						<div class="col-sm-4"><img src="tigre.jpg" class="img-thumbnail" style="width: 100%;"></div>
-						<div class="col-sm-8"><strong>Nom</strong><p><small>Simple description, easy peasy. Have fun one the website!</small></p></div>
+					<div class="panel-footer">
+						<div class="row">
+							<div class="col-sm-4">
+								<div class="profile_pic" style="background-image: url('tigre.jpg');"></div>
+							</div>
+							<div class="col-sm-8"><strong>Nom</strong><p><small>Simple description, easy peasy. Have fun one the website!</small></p></div>
+						</div>
 					</div>
 				</div>
-			</div>
+			</a>
 		</div>
 
-		<div class="col-sm-4"> 
-			<div class="panel panel-primary" style="background-color: #000000">
-				<!-- <div class="panel-heading">BLACK FRIDAY DEAL</div> -->
-				<video class="panel-body video" style="width: 100%; margin: -14.87px; margin-left: 50%; transform: translateX(-50%);" id="bgVideo" loop  muted="" preload autoplay>
-					<source src="video1.mp4" type="video/mp4"></source>
-				</video>
-				<!-- <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%; margin: -15px; margin-left: 50%; transform: translateX(-50%); " alt="Image"></div> -->
+		<div class="col-sm-4">
+			<a href="#" style="color: #000000">
+				<div class="panel panel-primary" style="background-color: #000000">
+					<video class="panel-body video" style="width: 100%; margin: -14.87px; margin-left: 50%; transform: translateX(-50%);" id="bgVideo" loop  muted="" preload autoplay>
+						<source src="video1.mp4" type="video/mp4"></source>
+					</video>
 
-				<div class="panel-footer">
-					<div class="row">
-						<div class="col-sm-4"><img src="tigre.jpg" class="img-thumbnail" style="width: 100%;"></div>
-						<div class="col-sm-8"><strong>Nom</strong><p><small>Simple description, easy peasy. Have fun one the website!</small></p></div>
+					<div class="panel-footer">
+						<div class="row">
+							<div class="col-sm-4">
+								<div class="profile_pic" style="background-image: url('tigre.jpg');"></div>
+							</div>
+							<div class="col-sm-8"><strong>Nom</strong><p><small>Simple description, easy peasy. Have fun one the website!</small></p></div>
+						</div>
 					</div>
 				</div>
-			</div>
+			</a>
 		</div>
+
 	</div>
 
 </div>
